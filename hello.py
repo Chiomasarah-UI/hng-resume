832120422c4e4f07266a6de8b318582c4a8f42ca
Chiomasarah-UI <82842906+Chiomasarah-UI@users.noreply.github.com>
2021-08-20 14:02:47 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+print("ABANA CHIOMA SARAH")
+
+Name = input("what is your name \n")
+
+Allname = ("ABANA", "chioma", "sarah")
+
+userID = Allname.index(Name)
+
+if(Name in Allname):
+
+    print("successful")
+else:
+    print("Try Again")
---
+++ resume/README.md
@@ -0,0 +1,2 @@
+# resume
+ hng stage2
